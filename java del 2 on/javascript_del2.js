08590895cba143341b248b606291f0abf13f990e
oscar1425 <122865544+oscar1425@users.noreply.github.com>
2023-03-10 13:16:25 +0100

    del2

---
+++ java del 2 on/javascript_del2.js
@@ -0,0 +1,126 @@
+// var x ="hej verden"
+// alert(x)
+
+// var x ="heej hvor den går det"
+// var y ="verden"
+// var sarmlet = x + y;
+// document.write(sarmlet)
+
+
+// var tal1 = 1; 
+// var tal2 = 3; 
+// var tallet = tal1 + tal2;
+// document.write(tallet)
+
+// var text ="dette er en tekst"
+// var tall = 1337
+// var samlet = text + tall 
+// document.write(samlet)
+
+// var tal1 = 1; 
+// var tal2 = 2; 
+// document.write(tal1 + tal2)
+// document.write(tal1 - tal2)
+// document.write(tal1 * tal2)
+// document.write(tal1 / tal2)
+
+// var tal = prompt("Skriv et tal");
+// document.write(tal)
+
+// var x = prompt("skiv noget");       
+// if (isNaN(x)) {
+//     document.write(x)
+    
+// }else{
+//     document.write("det var et tal: "+x)
+// }
+
+// var i = prompt("skriv et tal: ");
+// while(isNaN(i)){
+//     i = prompt("skriv et tal")
+// }
+
+// var et = "test" 
+// document.write(typeof(et))
+// var to = 1234
+// document.write(typeof(to))
+// var tre = true 
+// document.write(typeof(tre))
+
+// var opg13 = prompt ("god dag")
+// document.write(typeof(opg13))
+
+// var i = 1 
+// while(i < 5) 
+// { 
+//  document.write(i)
+//  i++; 
+// } 
+
+// var i = 1 
+// while(i < 5) 
+// { 
+//  if(i % 2 == 0){ 
+//         // indholdet af i er lige 
+//         document.write(i +"er lige<br>")
+//        } else { 
+//         // indholdet af i, er IKKE lige
+//         document.write(i +"er ulige")
+    
+//     }
+//     i++;
+// } 
+//opg16
+// var input = prompt("skiv noget");
+// if(isNaN(input)){  
+//             document.write("text")
+//            } else { 
+             
+//             if(input % 2 == 0){ 
+//                         // indholdet af i er lige 
+//                         document.write(input +"er lige<br>")
+//                        } else { 
+//                         // indholdet af i, er IKKE lige
+//                         document.write(input +"er ulige")
+                    
+//                     }
+        
+//         }
+//opg 17
+// var i = 1;
+// while (i<5) {
+//     document.write("<p>div</p>")
+//     i++;
+    
+//}
+
+//opg18
+// document.addEventListener("DOMContentLoaded", function() { 
+// var i=1
+// while(i < 5) {
+//     document.getElementById("et").innerHTML = "<p>test</p>"
+    
+//     i++
+// }
+// })
+
+// opg19
+// document.addEventListener("DOMContentLoaded", function() { 
+// var i=1
+// while(i < 5) {
+//     document.getElementById("et").innerHTML += "<p>test</p>"
+    
+//     i++
+// }
+// })
+
+
+
+
+
+
+
+
+
+
+
